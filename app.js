5f64a3a15550421a2176c58c7a84a50f8714dea3
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-07-03 19:24:46 +0300

    Préparation à la production

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ require('dotenv').config()
 
 app.use(bodyParser.json())
 
+// Configuration CORS
+const corsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+};
 app.use(cors(corsOptions));
 
 sequelize.initBdd()
--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -6,7 +6,6 @@ const PokemonModel = require('../models/Pokemon');
 const UserModel = require('../models/User')
 const POKEMONS = require('../db/constant/POKEMONS.JS');
 
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
